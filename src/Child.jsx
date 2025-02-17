e7b94e3ce5931a04a8e2679d4854f70b3baa14a9
Muhammad Mateen <liaqatmateen303@gmail.com>
2025-02-17 16:23:41 +0500

    add

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Child = ({ title }) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        console.log(`${count}`);
-    })
-    return (
-        <div className='flex justify-center mt-10'>
-            <div className='items-center'>
-                <p className='text-center py-6'>Counter : {count}</p>
-                <button className='border bg-blue-500 p-2 px-4 font-semibold rounded-lg' onClick={() => {
-                    setCount(count + 1)
-                }}>Increment +</button>
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    console.log(`${count}`);
+  });
+
+  return (
+
+    <div className="flex justify-center items-center md:mt-24  mt-10 md:px-4">
+      <div className="w-[70%] bg-white shadow-lg rounded-lg p-6 text-center">
+        <h1  className='text-center md:pt-7 pt-6 md:text-3xl text-2xl font-sans font-semibold'>{title}</h1>
+        <p className="text-lg font-semibold md:py-8 py-7">Counter: {count}</p>
+        <button
+          className="bg-blue-500 text-white font-semibold py-2 px-4 mb-8 rounded-lg transition duration-300 hover:bg-blue-600"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment +
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Child;
